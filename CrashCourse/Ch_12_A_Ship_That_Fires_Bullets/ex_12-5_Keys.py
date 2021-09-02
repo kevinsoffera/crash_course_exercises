5faa032edab12052cc8b0778e4a5f4a380e1a29f
Kevin Soffera <kevinsoffera@gmail.com>
2021-09-01 20:03:54 -0400

    Added exercise 12-5 Keys. Still need to figure out what exactly its asking for.

---
+++ CrashCourse/Ch_12_A_Ship_That_Fires_Bullets/ex_12-5_Keys.py
@@ -0,0 +1,33 @@
+import sys
+
+import pygame
+
+class Keys:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Keys")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    key_pressed = pygame.key.key_code
+                    print(key_pressed)
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = Keys()
+    ai.run_game()
+
